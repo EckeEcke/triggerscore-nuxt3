f8b2d8bd66f55744c90fff2b1e83698f75a94b2e
Christian Eckardt <eckardt.christian@web.de>
2023-05-22 20:40:18 +0200

    setting html lang

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
+  <Html :lang="locale"></Html>
   <div class="bg-gray-900 min-h-screen flex flex-col">
     <Header />
     <slot />
     <Footer />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useStore } from '~/stores/store'
+
+const store = useStore()
+const locale = computed(() => store.locale)
+
+</script>
 
 <style>
 .page-enter-active,
