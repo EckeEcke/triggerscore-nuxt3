cfe7e9b788a8a8a2c31b1e6d3db6d5edbeb8af4e
Christian Eckardt <eckardt.christian@web.de>
2023-05-31 23:09:49 +0200

    one more localstorage fix

--- stores/store.ts
+++ stores/store.ts
@@ -94,7 +94,7 @@ export const useStore = defineStore({
   },
   actions: {
     async setTriggerscores(){  
-        if(localStorage && localStorage.getItem('store')) {
+        if(process.client && localStorage.getItem('store')) {
             this.moviesLoading = false
         }                                               //also loads movies for now
         const scores = await fetch(url)
