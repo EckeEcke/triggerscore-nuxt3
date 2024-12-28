4fd4e6b5312c25fe5ef2843719444448a7740382
Christian Eckardt <eckardt.christian@web.de>
2024-12-28 12:11:28 +0100

    remove axios

--- netlify/functions/fetchMovies.js
+++ netlify/functions/fetchMovies.js
@@ -1,29 +1,36 @@
-import axios from 'axios'
 import { connectToDatabase } from './dbClient.js'
 
 export const handler = async (event) => {
-    try {
-        const url = new URL(event.rawUrl) 
-        const locale = url.searchParams.get('locale')
-        const database = await connectToDatabase()
-        const scores = database.collection('scores')
-        const movieIds = await scores.distinct('movie_id')
-        
-        const movieDataPromises = movieIds.map(id => 
-        axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=${locale}&append_to_response=videos`)
-        )
-        const movieDataResponses = await Promise.all(movieDataPromises)
-        const movies = movieDataResponses.map(response => response.data)
+  try {
+    const url = new URL(event.rawUrl)
+    const locale = url.searchParams.get('locale')
+    const database = await connectToDatabase()
+    const scores = database.collection('scores')
+    const movieIds = await scores.distinct('movie_id')
+    
+    const movieDataPromises = movieIds.map(id => 
+      fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=${locale}&append_to_response=videos`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`Error fetching movie data: ${response.statusText}`)
+          }
+          return response.json()
+        })
+    )
+    
+    const movieDataResponses = await Promise.all(movieDataPromises)
+    const movies = movieDataResponses.map(response => response)
 
-        return {
-        statusCode: 200,
-        body: JSON.stringify(movies)
-        }
-    } catch (error) {
-        console.error(error)
-        return {
-        statusCode: 500,
-        body: JSON.stringify({ message: 'Internal Server Error' })
-        }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(movies)
     }
+  } catch (error) {
+    console.error(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' })
+    }
+  }
 }
+
