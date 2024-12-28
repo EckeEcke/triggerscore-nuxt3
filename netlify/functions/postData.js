8a2f0fd4ea56ab0ae4b99825e1c95592ad6a3e63
Christian Eckardt <eckardt.christian@web.de>
2024-12-28 02:10:46 +0100

    try set cors headers

--- netlify/functions/postData.js
+++ netlify/functions/postData.js
@@ -2,52 +2,82 @@ import { connectToDatabase } from './dbClient.js'
 import { calculateTotal } from './calculateScores.js'
 
 export const handler = async (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Credentials': 'true',
+            },
+        }
+    }
+
     const body = JSON.parse(event.body)
     if (!body.movieID) {
         console.log("No movie defined in request body...")
         return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "Movie ID is required" })
+            statusCode: 400,
+            body: JSON.stringify({ message: "Movie ID is required" }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Credentials': 'true',
+            },
         }
     }
 
     try {
         const database = await connectToDatabase()
         const result = await database.collection('scores').insertOne({
-        movie_id: body.movieID,
-        rating_sexism: body.sexism,
-        rating_racism: body.racism,
-        rating_others: body.others,
-        rating_cringe: body.cringe,
-        rating_total: calculateTotal({
+            movie_id: body.movieID,
             rating_sexism: body.sexism,
             rating_racism: body.racism,
-            rating_others: body.others
-        }),
-        comment: body.comment,
-        liked: body.like,
-        disliked: body.dislike,
-        title: body.title,
-        original_title: body.original_title,
-        runtime: body.runtime,
-        vote_average: body.vote_average,
-        tagline: body.tagline,
-        overview: body.overview,
-        imdb_id: body.imdb_id,
-        backdrop_path: body.backdrop_path,
-        poster_path: body.poster_path,
-        release_date: body.release_date,
-        createdAt: new Date()
+            rating_others: body.others,
+            rating_cringe: body.cringe,
+            rating_total: calculateTotal({
+                rating_sexism: body.sexism,
+                rating_racism: body.racism,
+                rating_others: body.others
+            }),
+            comment: body.comment,
+            liked: body.like,
+            disliked: body.dislike,
+            title: body.title,
+            original_title: body.original_title,
+            runtime: body.runtime,
+            vote_average: body.vote_average,
+            tagline: body.tagline,
+            overview: body.overview,
+            imdb_id: body.imdb_id,
+            backdrop_path: body.backdrop_path,
+            poster_path: body.poster_path,
+            release_date: body.release_date,
+            createdAt: new Date()
         })
         return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Received request", result })
+            statusCode: 200,
+            body: JSON.stringify({ message: "Received request", result }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Credentials': 'true',
+            },
         }
     } catch (err) {
         console.error(err)
         return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "Error inserting data" })
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error inserting data" }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Credentials': 'true',
+            },
         }
     }
 }
