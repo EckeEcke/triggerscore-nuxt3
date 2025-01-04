63f8d87b91a75c29d11054ea673233d5662dbc0b
Christian Eckardt <eckardt.christian@web.de>
2025-01-04 11:52:47 +0100

    use comprimized quiz image

--- pages/Quiz.vue
+++ pages/Quiz.vue
@@ -93,7 +93,7 @@
       <h2 class="text-white text-xl font-semibold mb-4">{{ t("quiz.tagline") }}</h2>
       <hr class="my-6">
       <div class="flex flex-col gap-8 items-center align-center justify-center mb-8">
-        <img class="quiz-image max-w-16 hidden md:block" src="/images/quiz-image.png" />
+        <img class="quiz-image max-w-16 hidden md:block" src="/images/quiz-image-komprimiert.png" />
         <p class="text-white text-lg text-wrap-balance">
           {{ t("quiz.description") }}
         </p>
