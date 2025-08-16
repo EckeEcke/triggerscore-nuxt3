27bbc826e2ffcfa606bb982e9449b32d78f46cf0
Christian Eckardt <christian.eckardt@tchibo.de>
2025-08-16 13:13:57 +0200

    make whole body black

--- layouts/default.vue
+++ layouts/default.vue
@@ -57,16 +57,9 @@ onUnmounted(() => {
   document.removeEventListener('MSFullscreenChange', onFullscreenChange)
 })
 
+useHead({
+  bodyAttrs: {
+    class: 'bg-gray-900'
+  }
+})
 </script>
-
-<style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-</style>
