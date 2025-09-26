9062cc0ba47aad96ec6d12b0aa433ff17698207f
Christian Eckardt <christian.eckardt@tchibo.de>
2025-09-26 15:02:13 +0200

    remove middleware and success page, living with netlify default success page for now

--- middleware/redirect.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-    if (to.path === '/success') {
-        const { $i18n } = useNuxtApp()
-        const locale = $i18n.locale.value || 'de'
-
-        return navigateTo(`/${locale}/success`, { redirectCode: 302 })
-    }
-})
