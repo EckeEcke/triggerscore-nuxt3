128795679562822cf100fd36379bf30693763491
Christian Eckardt <christian.eckardt@tchibo.de>
2025-09-26 14:01:35 +0200

    add success page and missing translations

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -14,6 +14,7 @@
           method="POST"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
+          action="/success"
           @submit="handleSubmit"
       >
         <div class="hidden">
