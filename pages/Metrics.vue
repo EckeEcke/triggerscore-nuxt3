18d3b3851045c12ea9d96b2afa3edb78e69ceca8
Christian Eckardt <christian.eckardt@tchibo.de>
2025-11-11 12:55:40 +0100

    adjust used fonts

--- pages/Metrics.vue
+++ pages/Metrics.vue
@@ -35,7 +35,7 @@ function loadVisitCounts() {
           path: page.path,
           append: `#visit-count-${page.id}`,
           no_branding: true,
-          style: '* {color: #000; font-size: 14px;font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol" !important;border: none}'
+          style: '* {color: #000; font-size: 14px;font-family: Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif;border: none!important}'
         })
       })
     }
